d1f0e4433f3e074eb662d55d7527d595af79cfe5
KK <kamlesh.kumar@advantev.com>
2026-01-30 11:12:03 +0530

    update config

--- server/src/main.ts
+++ server/src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
   logger.log(`🚀 Application is running on: http://localhost:${port}`);
